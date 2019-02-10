16d32cb1e131f9fa658fcefa8c711ec529065900
Malik Iscandari <miscandari@mail.sfsu.edu>
2019-02-10 13:55:04 -0800

    Final Commit

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController , AVAudioPlayerDelegate{
     
     
     var audioPlayer : AVAudioPlayer!
+    var selectedSoundFileName: String = " "
     let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"] // created a file array containg all the audio files
 
     override func viewDidLoad() {
@@ -23,14 +24,13 @@ class ViewController: UIViewController , AVAudioPlayerDelegate{
 
     @IBAction func notePressed(_ sender: UIButton) {
         
-        var selectedSoundFileName : String = soundArray[sender.tag - 1] // recall that each button has an associated interger tag, so we made use of the feature in this array!
-        print(selectedSoundFileName)
+        selectedSoundFileName = soundArray[sender.tag - 1] // recall that each button has an associated interger tag, so we made use of the feature in this array!
         playSound()
 
     }
     
     func playSound(){
-        let soundURL = Bundle.main.url(forResource: "note1" , withExtension: "wav") // creating a path to out file name
+        let soundURL = Bundle.main.url(forResource: selectedSoundFileName , withExtension: "wav") // creating a path to out file name
         
         do { // the the do is what you are trying to do
             audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
